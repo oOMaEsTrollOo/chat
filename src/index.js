99a0f2e5288b140b2109378e121fc06c38d41724
oOMaEsTrollOo <denisov_den777@mail.ru>
2022-03-07 21:24:21 +0500

    lesson 1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { App } from "./App";
+// import { App } from "./App";
+
+function Message(props) {
+  return <h1 className="hello">Здравствуйте, {props.name}</h1>;
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode>{/* <App /> */}</React.StrictMode>,
   document.getElementById("root")
 );
+
+const App = <Message name="Богдан" />;
+ReactDOM.render(App, document.getElementById("root"));
