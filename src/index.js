a8defdebe966b7200878d6e24ce5d60ca8bb0b08
oOMaEsTrollOo <denisov_den777@mail.ru>
2022-03-29 19:32:45 +0500

    lesson 2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,82 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-// import { App } from "./App";
+import React, {
+  useState,
+  useEffect,
+  // useLayoutEffect,
+  // useCallback,
+  // useMemo,
+  // useRef,
+} from "react";
 
-function Message(props) {
-  return <h1 className="hello">Здравствуйте, {props.name}</h1>;
-}
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const MessageList = () => {
+  const [value, setValue] = useState("");
+  const [messages, setMessages] = useState([
+    {
+      author: "Bot",
+      message: "message 1",
+      date: new Date().toLocaleDateString(),
+    },
+  ]);
+
+  const sendMessages = () => {
+    setMessages([...messages, { author: "User", message: value }]);
+    setValue("");
+  };
+
+  useEffect(() => {
+    const lastMessages = messages[messages.length - 1];
+    let timerId = null;
+
+    if (messages.length && lastMessages.author === "User") {
+      timerId = setTimeout(() => {
+        setMessages([
+          ...messages,
+          { author: "Bot", message: "hello from bot" },
+        ]);
+      }, 500);
+    }
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [messages]);
+
+  return (
+    <div>
+      <h2>
+        <input
+          placeholder="Введите сообщение"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={sendMessages}>send message</button>
+        <hr />
+        {messages.map((message) => (
+          <div>
+            <h2>{message.author}</h2>
+            <p>{message.message}</p>
+            <hr />
+          </div>
+        ))}
+      </h2>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <MessageList />
+    </>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>{/* <App /> */}</React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
-
-const App = <Message name="Богдан" />;
-ReactDOM.render(App, document.getElementById("root"));
